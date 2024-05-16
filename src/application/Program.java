e1cc3c317e432a45143a23842730baa1034cd095
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-05-16 14:20:04 -0300

    Outra solução imprópria

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,9 +16,7 @@ public class Program {
 		myShapes.add(new Circle(2.0));
 
 		/*
-		 * Total area vai funcionar somente com myShapes. Ao criarmos uma lista de
-		 * Circles, não consegue buscar a total area de myCircles, pois Shape (totalArea(List<Shape> list))não é um
-		 * super tipo de Circle.Portanto, total area vai funcionar somente pra lista de Shape. Para funcionar vai precisar de um tipo Curinga
+		 * Para funcionar vai precisar de um tipo Curinga:
 		 */
 
 		List<Circle> myCircle = new ArrayList<>();
@@ -27,7 +25,11 @@ public class Program {
 		System.out.println("Total area: " + totalArea(myCircle));
 	}
 
-	public static double totalArea(List<Shape> list) {
+	/*
+	 * Se colocar somente "?", list da erro ( for (Shape s : list) { ) pois pode
+	 * ser que nem todos da lista são do tipo shape. 
+	 */
+	public static double totalArea(List<?> list) {
 		double sum = 0.0;
 		for (Shape s : list) {
 			sum += s.area();
