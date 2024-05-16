94521801f8e3699b2b0f86bbd757be1f3d4fff40
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-05-16 14:17:00 -0300

    Observações

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,21 +11,22 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		/*
-		 * Adicionou-se duas figuras (rectangle e circle) na lista de Shape. Total area
-		 * vai somar o valor da area do rectangle e a area do circle.
-		 * 
-		 */
 		List<Shape> myShapes = new ArrayList<>();
 		myShapes.add(new Rectangle(3.0, 2.0));
 		myShapes.add(new Circle(2.0));
 
-		System.out.println("Total area: " + totalArea(myShapes));
+		/*
+		 * Total area vai funcionar somente com myShapes. Ao criarmos uma lista de
+		 * Circles, não consegue buscar a total area de myCircles, pois Shape (totalArea(List<Shape> list))não é um
+		 * super tipo de Circle.Portanto, total area vai funcionar somente pra lista de Shape. Para funcionar vai precisar de um tipo Curinga
+		 */
+
+		List<Circle> myCircle = new ArrayList<>();
+		myCircle.add(new Circle(2.0));
+		myCircle.add(new Circle(3.0));
+		System.out.println("Total area: " + totalArea(myCircle));
 	}
 
-	/*
-	 * o método totalArea vai receber uma lista de shape, e fazer a soma.
-	 */
 	public static double totalArea(List<Shape> list) {
 		double sum = 0.0;
 		for (Shape s : list) {
