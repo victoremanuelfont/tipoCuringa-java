6892b8f66dd17e6d21b677178c3bd2a50e6098d8
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-05-16 14:26:50 -0300

    Uso do tipo Curinga

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,10 +15,6 @@ public class Program {
 		myShapes.add(new Rectangle(3.0, 2.0));
 		myShapes.add(new Circle(2.0));
 
-		/*
-		 * Para funcionar vai precisar de um tipo Curinga:
-		 */
-
 		List<Circle> myCircle = new ArrayList<>();
 		myCircle.add(new Circle(2.0));
 		myCircle.add(new Circle(3.0));
@@ -26,10 +22,16 @@ public class Program {
 	}
 
 	/*
-	 * Se colocar somente "?", list da erro ( for (Shape s : list) { ) pois pode
-	 * ser que nem todos da lista são do tipo shape. 
+	 * Isso quer dizer que a list pode ser do tipo shape ou de qualquer outro
+	 * subtipo de shape. Desse modo, aceita myCircle. Porém, desse modo, não da para
+	 * adicionar.
 	 */
-	public static double totalArea(List<?> list) {
+
+	public static double totalArea(List<? extends Shape> list) {
+		/*
+		 * list.add(new Rectangle(3.0,4.0) Não da para fazer isso pois pode ser um
+		 * subtipo de shape PORÉM incompatível com Rectangle
+		 */
 		double sum = 0.0;
 		for (Shape s : list) {
 			sum += s.area();
